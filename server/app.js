8d718cf0188aa16bee969260f0c417ed096ec4d6
Cory Moore <cory@corymoore.com>
2019-05-03 21:27:56 -0500

    added mongo and mongoose

--- server/app.js
+++ server/app.js
@@ -2,6 +2,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+
+var mongoDB = 'mongodb://wozu:1tester@ds151416.mlab.com:51416/xs-records';
+
+mongoose.connect(mongoDB);
+
+var db = mongoose.connection;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
