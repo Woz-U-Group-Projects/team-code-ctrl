72e89869cb543b892224e3f87493a1936a2b47f8
Kyle <kyle.black@my.scitexas.edu>
2019-05-06 18:28:51 -0500

    Got the login page to render on root route. No proper routing yet.

--- client/xs-records/src/App.js
+++ client/xs-records/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
+import LoginPage from '../src/login';
 
 class App extends Component {
 
@@ -11,19 +13,25 @@ class App extends Component {
     };
   }
 
-  componentDidMount() {
-    fetch('/users/5ccda8748575fd03028a2cd7')
-    .then(res => res.json())
-    .then(data => this.setState({users: data}));
-  }
+  // componentDidMount() {
+  //   fetch('/users/5ccda8748575fd03028a2cd7')
+  //   .then(res => res.json())
+  //   .then(data => this.setState({users: data}));
+  // }
 
   render() {
     return (
       <div className="App">
         <h1>Users</h1>
         <ul>
-         <li key={this.state.users._id}>{this.state.users.email}</li>
+        <li key={this.state.users._id}>{this.state.users.email}</li>
         </ul>
+    <Router>
+          <div>
+            {/* <Route exact path="/" component={ Home } /> */}
+            <Route path="/login" component={ LoginPage } />
+          </div>
+        </Router>
       </div>
     );
   }
--- server/routes/users.js
+++ server/routes/users.js
@@ -3,40 +3,38 @@ const router = express.Router();
 const User = require('../models/userModel');
 
 
-router.get('/login', function(req, res, next) {
-  res.json([
-    {
-      id: 1,
-      email: "joe@blow.com"
-    },
-    {
-      id: 2,
-      email: "idiot@aol.com"
-    }
-  ]);
-});
+// router.get('/login', function(req, res, next) {
+//   res.json([
+//     {
+//       id: 1,
+//       email: "joe@blow.com"
+//     },
+//     {
+//       id: 2,
+//       email: "idiot@aol.com"
+//     }
+//   ]);
+// });
 
 /* GET route that retrieves a list of users */
 router.get('/:id', function(req, res, next) {
-  User.findById(req.params.id, function(err, user) {
-    if(err) return next(err);
-    res.json(user);
-  });
+    User.findById(req.params.id, function(err, user) {
+        if (err) return next(err);
+        res.json(user);
+    });
 });
 
-router.post('/register', function(req, res, next){
-  let user = new User(
-    {
-      email: req.body.email,
-      username: req.body.username
-    }
-  );
-  user.save(function(err) {
-    if(err) {
-      return next(err);
-    }
-    res.send('User Registered Successfully');
-  })
+router.post('/register', function(req, res, next) {
+    let user = new User({
+        email: req.body.email,
+        username: req.body.username
+    });
+    user.save(function(err) {
+        if (err) {
+            return next(err);
+        }
+        res.send('User Registered Successfully');
+    })
 });
 
 module.exports = router;
