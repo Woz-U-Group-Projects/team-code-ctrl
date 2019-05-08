7a02ad2e2cadd827573674735d03c4acb3bb4a6e
Cory Moore <cory@corymoore.com>
2019-05-08 18:59:52 -0500

    got login working, sorta

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/userModel');
+const bcrypt = require('bcrypt');
 
+router.post('/login', function(req, res, next) {
 
-router.get('/login', function(req, res, next) {
-  res.json([
-    {
-      id: 1,
-      email: "joe@blow.com"
-    },
-    {
-      id: 2,
-      email: "idiot@aol.com"
+  User.findOne({username: req.body.username}, function(err, user) {
+    if(user) {
+      bcrypt.compare(req.body.password, user.password, function() {
+        res.send({loggedIn: true});
+      }); 
+    } else {
+      console.error('oops');
     }
-  ]);
+  });
 });
 
 /* GET route that retrieves a list of users */
@@ -24,19 +24,28 @@ router.get('/:id', function(req, res, next) {
   });
 });
 
+
 router.post('/register', function(req, res, next){
+
+  let password = bcrypt.hashSync(req.body.password, 10);
+
   let user = new User(
     {
+      fname: req.body.fname,
+      lname: req.body.lname,
       email: req.body.email,
-      username: req.body.username
+      username: req.body.username,
+      password: password
     }
-  );
+  )
+
   user.save(function(err) {
     if(err) {
       return next(err);
     }
     res.send('User Registered Successfully');
   })
+
 });
 
 module.exports = router;
