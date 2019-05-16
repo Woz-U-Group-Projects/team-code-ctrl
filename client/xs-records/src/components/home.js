05fcee06446bc3c7dffb7063bf7bc3cb93a77be1
Kyle <kyle.black@my.scitexas.edu>
2019-05-15 20:23:02 -0500

    Merged Changes from last two commits
    The previous commit accidentally broke some minor changes. All changes are now together.

--- client/xs-records/src/components/home.js
+++ client/xs-records/src/components/home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Container, Row, Card, CardBody, CardTitle, CardText, CardImg, Jumbotron, Col, Button } from 'reactstrap';
 
 
+
 class Home extends Component {
 
   render() {
@@ -15,10 +16,36 @@ class Home extends Component {
 //           </div>
 //         </div>
 //       </Container>
-<Jumbotron >
-<h1 className="display-3 font-weight-bold text-center">XSR</h1>
-<p className="text-center">XS Records is the easiest way for musicians to get music into Spotify, iTunes, Amazon, Google Play, Tidal, and more.</p>
-</Jumbotron>
+            <Container>
+              <div >
+                <div>
+                  <Jumbotron >
+                    <h1 className="display-3 font-weight-bold text-center">XSR</h1>
+                    <p className="text-center">XS Records is the easiest way for musicians to get music into Spotify, iTunes, Amazon, Google Play, Tidal, and more.</p>
+                  </Jumbotron>
+                </div>
+              </div>
+              <div>
+                <Col sm={ { size: 'auto', offset: -2 } } >
+                  <Card>
+                    <CardBody>
+                      <CardTitle className="text-center font-weight-bold">XSR News</CardTitle>
+
+                      <CardText> <p>Id anim fugiat fugiat proident aute incididunt et irure enim cillum aliqua ullamco. Consectetur nostrud sit est non ex. Quis anim et occaecat culpa pariatur eiusmod.
+<br /><br />
+                        Officia dolor enim excepteur mollit in ea aliquip. Nisi reprehenderit amet ad excepteur esse deserunt. Ipsum dolor ad irure aute sit adipisicing ex non consectetur incididunt minim. Officia cillum nulla officia non. Magna cillum ex dolor nisi ad commodo enim et quis. Sit culpa occaecat in et tempor cillum.
+<br /><br />
+                        Aute dolor esse consequat in nulla et nulla sit. Cupidatat minim do aute consequat cupidatat labore exercitation voluptate. Magna exercitation enim culpa nisi et proident. Nisi excepteur consectetur ea dolore. Sunt minim veniam incididunt velit.</p>
+                      </CardText>
+
+                    </CardBody>
+                  </Card>
+                </Col>
+              </div>
+
+            </Container>
+
+
 
     )
   }
